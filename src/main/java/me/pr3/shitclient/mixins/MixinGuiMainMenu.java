513e120b64acc6e58bfac4343068ae379e00e2d2
Pr3roxDLC <57639545+Pr3roxDLC@users.noreply.github.com>
2020-09-23 00:46:51 +0200

    Started Working on a Custom Main Menu Background, broken atm

--- src/main/java/me/pr3/shitclient/mixins/MixinGuiMainMenu.java
+++ src/main/java/me/pr3/shitclient/mixins/MixinGuiMainMenu.java
@@ -1,8 +1,13 @@
 package me.pr3.shitclient.mixins;
 
+import me.pr3.shitclient.Main;
 import me.pr3.shitclient.utils.Log;
 import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.util.ResourceLocation;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -10,10 +15,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GuiMainMenu.class)
 public class MixinGuiMainMenu {
 
+    @Mutable
+    @Shadow @Final private static ResourceLocation[] TITLE_PANORAMA_PATHS;
+
     @Inject(method = "<init>", at = @At("RETURN"))
     public void initMainMenu(CallbackInfo callbackInfo){
 
         Log.info("Main Menu Initialized!!");
+
+        TITLE_PANORAMA_PATHS[0] = new ResourceLocation("/panoramaNorth.png");
+        TITLE_PANORAMA_PATHS[1] = new ResourceLocation("/panoramaNorth.png");
+        TITLE_PANORAMA_PATHS[2] = new ResourceLocation("/panoramaNorth.png");
+        TITLE_PANORAMA_PATHS[3] = new ResourceLocation("/panoramaNorth.png");
+        TITLE_PANORAMA_PATHS[4] = new ResourceLocation("/panoramaNorth.png");
+        TITLE_PANORAMA_PATHS[5] = new ResourceLocation("/panoramaNorth.png");
+
 
     }
 
