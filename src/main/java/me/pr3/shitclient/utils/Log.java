5789bded46344f6d442e1926f30764e1d64bd3b7
blockparole <48992448+blockparole@users.noreply.github.com>
2020-09-24 18:53:10 +0200

    localchat setting for Log

--- src/main/java/me/pr3/shitclient/utils/Log.java
+++ src/main/java/me/pr3/shitclient/utils/Log.java
@@ -1,32 +1,73 @@
 package me.pr3.shitclient.utils;
 
+import com.mojang.realmsclient.gui.ChatFormatting;
 import me.pr3.shitclient.Main;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.text.ChatType;
+import net.minecraft.util.text.TextComponentString;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.text.MessageFormat;
+
 public class Log {
 
+    private static final Minecraft mc = Minecraft.getMinecraft();
     private static final Logger logger = LogManager.getLogger(Main.MOD_ID);
 
-    public static void log(Level level, String message) {
-        logger.log(level, message);
+    public static void debug(String message) {
+        debug(message, false);
     }
 
-    public static void debug(String message) {
+    public static void debug(String message, boolean chat) {
         log(Level.DEBUG, message);
+        if (chat) {
+            sendLocalChat(message);
+        }
     }
 
     public static void info(String message) {
+        info(message, false);
+    }
+
+    public static void info(String message, boolean chat) {
         log(Level.INFO, message);
+        if (chat) {
+            sendLocalChat(message);
+        }
     }
 
     public static void warn(String message) {
+        warn(message, false);
+    }
+
+    public static void warn(String message, boolean chat) {
         log(Level.WARN, message);
+        if (chat) {
+            sendLocalChat(message);
+        }
     }
 
     public static void error(String message) {
+        error(message, false);
+    }
+
+    public static void error(String message, boolean chat) {
         log(Level.ERROR, message);
+        if (chat) {
+            sendLocalChat(message);
+        }
+    }
+
+    private static void log(Level level, String message) {
+        logger.log(level, message);
+    }
+
+    private static void sendLocalChat(String message) {
+        if (mc.player != null && mc.world != null) {
+            mc.ingameGUI.addChatMessage(ChatType.CHAT, new TextComponentString(MessageFormat.format("{0}{1}{2} {3}{4} {5}", ChatFormatting.BOLD, ChatFormatting.DARK_RED, "SHIT", "->", ChatFormatting.RESET, message)));
+        }
     }
 
 }
