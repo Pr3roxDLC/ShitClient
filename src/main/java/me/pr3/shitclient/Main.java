e7fe3f1c45e09bfbbdcc11f64efdec1f1349dda5
blockparole <48992448+blockparole@users.noreply.github.com>
2020-09-24 16:56:26 +0200

    log

--- src/main/java/me/pr3/shitclient/Main.java
+++ src/main/java/me/pr3/shitclient/Main.java
@@ -2,9 +2,9 @@ package me.pr3.shitclient;
 
 
 import com.google.common.eventbus.EventBus;
-import me.pr3.shitclient.events.EventManager;
-import me.pr3.shitclient.modules.ModuleManager;
-import me.pr3.shitclient.utils.CommandHandler;
+import me.pr3.shitclient.plumbing.CommandHandler;
+import me.pr3.shitclient.plumbing.EventManager;
+import me.pr3.shitclient.plumbing.ModuleManager;
 import me.pr3.shitclient.utils.Log;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
@@ -13,37 +13,35 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "shitclient", name = "shitclient", version = "1.0.0" )
+@Mod(modid = "shitclient", name = "shitclient", version = "1.0.0")
 public class Main {
 
-	public static final String MOD_ID = "shitclient";
+    public static final String MOD_ID = "shitclient";
 
-	public static final EventBus BUS = new EventBus(MOD_ID);
+    public static final EventBus BUS = new EventBus(MOD_ID);
 
-	public static ModuleManager mm = new ModuleManager();
+    public static ModuleManager mm = new ModuleManager();
 
-	@EventHandler
-	public  void PreInit(FMLPreInitializationEvent event) {
+    @EventHandler
+    public void PreInit(FMLPreInitializationEvent event) {
+        Log.info("FMLPreInitialization");
+    }
 
-		Log.setLogger(event.getModLog());
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
 
-	}
+        MinecraftForge.EVENT_BUS.register(new EventManager());
+        BUS.register(new CommandHandler());
 
-	@EventHandler
-	public void init(FMLInitializationEvent event) {
+        BUS.register(mm);
 
-		MinecraftForge.EVENT_BUS.register(new EventManager());
-		BUS.register(new CommandHandler());
+        mm.initModules();
 
-		BUS.register(mm);
+    }
 
-		mm.initModules();
-
-	}
-
-	@EventHandler
-	public void PostInit(FMLPostInitializationEvent event) {
-
-	}
+    @EventHandler
+    public void PostInit(FMLPostInitializationEvent event) {
+        Log.info("FMLPostInitializationEvent");
+    }
 
 }
